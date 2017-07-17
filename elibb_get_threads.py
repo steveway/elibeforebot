aaadb7170660136394844f7e2491bd9fc2107054
Stefan Murawski <steveway1@googlemail.com>
2017-07-17 16:26:22 +0200

    Save data in SQLite Database instead of CSV.

--- elibb_get_threads.py
+++ elibb_get_threads.py
@@ -4,21 +4,40 @@ import sys
 import pprint
 import csv
 import os
+import sqlite3
 from shorttext.utils import standard_text_preprocessor_1
 
 
 def manualsortsubs(listofsubs):
     preprocessor1 = standard_text_preprocessor_1()
-    choices = ["nothing", "askedbefore", "premisewrong"] #TODO: decide on choices
+    choices = ["nothing", "askedbefore", "premisewrong"]  # TODO: decide on choices
     title = "Choose category"
     classdict = {"nothing": [], "askedbefore": [], "premisewrong": []}
     for sub in listofsubs:
-        choice = easygui.buttonbox(sub, title, choices)
+        choice = easygui.buttonbox(sub[0], title, choices)
         if choice is None:
             sys.exit(0)
         else:
-            classdict[choice].append(preprocessor1(sub))
+            classdict[choice].append([preprocessor1(sub[0]), sub[1]])
     return classdict
+
+
+def write_to_sqlite(sorted_dict):
+    conn = sqlite3.connect('prepdata.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute('''create table if not exists submissions (submission text, category text, id text, likes integer)''')
+    for key, value in sorted_dict.items():
+        for subitem in value:
+            # check if submission already exists
+            cur.execute('''select * from submissions where id = ?''', [subitem[1]])
+            res = cur.fetchall()
+            print(res)
+            if not res:
+                cur.execute('''insert into submissions values (?,?,?,?)''', (subitem[0], key, subitem[1], 0))
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 def write_to_csv(sorted_dict):
@@ -26,7 +45,26 @@ def write_to_csv(sorted_dict):
         writer = csv.writer(csv_file, delimiter=",", lineterminator='\n')
         for key, value in sorted_dict.items():
             for subitem in value:
-                writer.writerow([key, subitem])
+                writer.writerow([key, subitem[0]])
+
+
+def read_from_sqlite(fpath="prepdata.db"):
+    classdict = {}
+    conn = sqlite3.connect(fpath)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute('''select * from submissions''')
+    res = cur.fetchall()
+    for row in res:
+        try:
+            classdict[row["category"]].append(row["submission"])
+        except KeyError:
+            classdict[row["category"]] = [row["submission"]]
+
+    pp = pprint.PrettyPrinter(indent=4)
+    for row in res:
+        pp.pprint(dict(row))
+    pp.pprint(classdict)
 
 
 def read_from_csv(fpath="dict.csv"):
@@ -71,17 +109,22 @@ def get_submissions(sublimit=None):
         unsortedsubs = []
         for rethread in elisub.new(limit=sublimit):  # limit=None
             if rethread.id not in already_done:
-                unsortedsubs.append(rethread.title + " " + rethread.selftext)
+                unsortedsubs.append([rethread.title + " " + rethread.selftext, rethread.id])
                 already_done.add(rethread.id)
         classdict = manualsortsubs(unsortedsubs)
-        write_to_csv(classdict)
+        # write_to_csv(classdict)
+        write_to_sqlite(classdict)
         return classdict
 
 
 def main():
-    classdict = get_submissions(25)
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(classdict)
+    # conn = sqlite3.connect('prepdata.db')
+    # conn.row_factory = sqlite3.Row
+    # cur = conn.cursor()
+    classdict = get_submissions(2)
+    read_from_sqlite()
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(classdict)
 
 
 if __name__ == "__main__":
